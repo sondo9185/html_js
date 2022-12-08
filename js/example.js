700894c639bd396804b0a58ea09f0de6917869a4
sondongwoo <sondo9185@naver.com>
2022-12-08 15:00:08 +0900

    js파일에 css파일에 modal 추가

--- js/example.js
+++ js/example.js
@@ -1,6 +1,8 @@
 const nameH1Elemnent = document.querySelector('h1.inline');
 const connectNameElement = document.querySelector('span.red');
 
+const inputModalElement = document.querySelector('#inputModal');
+
 const setUserName = (name) => {
   nameH1Elemnent.textContent = name;
   connectNameElement.textContent = name;
@@ -13,13 +15,18 @@ nameH1Elemnent.textContent = localName;
 
 
 nameH1Elemnent.oncick = () => {
-  const inputName = prompt('이름을 입력해주세요.');
-  if (inputName) {
-    localStorage.setItem('name', inputName);
-
-    setUserName(inputName);
-  } else {
-    alert('이름이 입력되지 않았습니다.');
-  }
+  inputModalElement.showModal();
+  //  const inputName = prompt('이름을 입력해주세요.');
+  //  if (inputName) {
+  //    localStorage.setItem('name', inputName);
+  //
+  //    setUserName(inputName);
+  //  } else {
+  //    alert('이름이 입력되지 않았습니다.');
+  //  }
 };
 
+inputModalElement.onclick = (event) => {
+  if (event.target.nodeName === 'DIALOG') inputModalElement.closest();
+  console.log(event.target.nodeName);
+};
