009d88f72f07817b8d939d72bf93193fdaf36162
sondongwoo <sondo9185@naver.com>
2022-12-08 14:30:48 +0900

    js 파일에 이름관련하여 업데이트

--- js/example.js
+++ js/example.js
@@ -1,5 +1,23 @@
 const nameH1Elemnent = document.querySelector('h1.inline');
+const connectNameElement = document.querySelector('span.red');
+
+const localName = localStorage.getItem('name');
+if (localName) {
+  nameH1Elemnent.textContent = localName;
+  connectNameElement.textContent = localName;
+}
+nameH1Elemnent.textContent = localName;
+
 
 nameH1Elemnent.oncick = () => {
-  alert('클릭되었습니다.');
+  const inputName = prompt('이름을 입력해주세요.');
+  if (inputName) {
+    localStorage.setItem('name', inputName);
+
+    nameH1Elemnent.textContent = inputName;
+    connectNameElement.textContent = inputName;
+  } else {
+    alert('이름이 입력되지 않았습니다.');
+  }
 };
+
