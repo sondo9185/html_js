eadbeada272deb98ffbce67ce16c5276740b39ab
sondongwoo <sondo9185@naver.com>
2022-12-08 16:36:20 +0900

    실행이 되지않는 학번과 이메일

--- js/example.js
+++ js/example.js
@@ -1,30 +1,33 @@
 const nameH1Elemnent = document.querySelector('h1.inline');
 const connectNameElement = document.querySelector('span.red');
-
+console.log(nameH1Elemnent);
 const inputModalElement = document.querySelector('#inputModal');
+const studentNoElement = document.querySelector('label.studentNo');
+const emailElement = document.querySelector('label.email');
 
 const setUserName = (name) => {
   nameH1Elemnent.textContent = name;
   connectNameElement.textContent = name;
 };
-
+const setstudentNo = (number) => {
+  studentNoElement.textContent = number;
+};
+const setemail = (email) => {
+  emailElement.textContent = email;
+};
 
 const localName = localStorage.getItem('name');
 if (localName) setUserName(localName);
-//  nameH1Elemnent.textContent = localName;
 
-//  console.log(nameH1Elemnent);
+const localnumber = localStorage.getItem('number');
+if (localnumber) setstudentNo(localnumber);
+
+const localemail = localStorage.getItem('number');
+if (localemail) setstudentNo(localemail);
+
+console.log(inputModalElement);
 nameH1Elemnent.onclick = () => {
-  //  console.log(inputModalElement);
   inputModalElement.showModal();
-  //  const inputName = prompt('이름을 입력해주세요.');
-  //  if (inputName) {
-  //    localStorage.setItem('name', inputName);
-  //
-  //    setUserName(inputName);
-  //  } else {
-  //    alert('이름이 입력되지 않았습니다.');
-  //  }
 };
 
 const modalSubmitBtn = document.querySelector('button.modalSubmit');
@@ -35,13 +38,14 @@ modalSubmitBtn.onclick = () => {
 
   for (const [key, value] of formData) {
     localStorage.setItem(key, value);
-    if (key === 'userName') setUserName(value);
-    //  console.log('${key}: ${value}');
+    if (key==='userName') setUserName(value);
+    else if (key==='studentNo') setstudentNo(value);
+    else if (key==='email') setemail(value);
   }
+
   inputModalElement.close();
 };
 
 inputModalElement.onclick = (event) => {
   if (event.target.nodeName === 'DIALOG') inputModalElement.close();
-  //  console.log(event.target.nodeName);
 };
